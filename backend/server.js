017923f94761752a4e5f36adfb3e8fd6f761e5c0
İbrahim Ethem Dartar <ibarhimethem4748@gmail.com>
2025-07-09 17:18:05 +0300

    server.js güncellendi

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const path = require('path'); // path modülünü ekleyin
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000; // Render'ın dinamik portunu kullanın, yoksa 3000
+const port = process.env.PORT || 10000; // Render'ın dinamik portunu kullanın, yoksa 10000
 
 // Statik dosyaların (HTML, CSS, JS, görseller vb.) bulunduğu dizini belirtin
-// Bu satır, public veya proje kök klasörünüzdeki dosyaları sunar.
-// Eğer index.html ve style.css ana klasördeyse, '.' kullanın.
-// Eğer statik dosyalarınız 'public' adında bir klasördeyse, 'public' kullanın.
-app.use(express.static(path.join(__dirname, '.'))); // Ana klasörü statik olarak sunar
+// Bu satır, server.js'in bulunduğu dizinden BİR ÜST DİZİNİ statik olarak sunar.
+app.use(express.static(path.join(__dirname, '..'))); // '..' bir üst dizini temsil eder
 
 // Herhangi bir route'a gelen istekler için (örneğin ana sayfa için '/')
 // index.html dosyasını gönder.
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'index.html')); // '..' ile bir üst dizindeki index.html'i bul
 });
 
 // Sunucuyu başlatma
